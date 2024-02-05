5da3eb46601723d72f255ca13a4ce86255d8137e
Tewodros01 <tewodrosm01@gmail.com>
2024-02-05 23:27:23 +0300

    AddNewFunctionality Genrs

--- src/app/features/songs/components/Loading.tsx
+++ src/app/features/songs/components/Loading.tsx
@@ -8,6 +8,7 @@ const progress = keyframes`
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
@@ -24,12 +25,17 @@ const ProgressBar = styled.div`
 const ProgressFill = styled.div`
   height: 100%;
   width: 0%;
-  background-color: #007bff;
+  background-color: #4b0082; /* Change color to water-like color */
   animation: ${progress} 2s linear infinite;
+`;
+
+const LoadingText = styled.div`
+  margin-top: 10px; /* Add margin to separate from progress bar */
 `;
 
 const Loading: React.FC = () => (
   <Container>
+    <LoadingText>Loading...</LoadingText>
     <ProgressBar>
       <ProgressFill />
     </ProgressBar>
