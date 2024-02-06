00b75b9dc40e735ea5063546567c6a840abcdfbf
Tewodros01 <tewodrosm01@gmail.com>
2024-02-06 16:07:12 +0300

    Root Reducer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,87 @@
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "./components/Layout";
-import Home from "./features/Home/components/Home";
-import AddSongForm from "./features/songs/components/AddSongForm";
-import EditSongForm from "./features/songs/components/EditSong";
-import ArtistsComponentWrapper from "./features/songs/components/ArtistsComponentWrapper";
-import AlbumsComponentWrapper from "./features/songs/components/AlbumsComponentWrapper";
-import GenresComponentWrapper from "./features/songs/components/GenresComponentWrapper";
-import SongListsComponentWrapper from "./features/songs/components/SongListsComponentWrapper";
 import NotFoundPage from "./components/NotFoundPage";
 
-const App = () => {
+const Home = lazy(() => import("./features/Home/components/Home"));
+const AddSongForm = lazy(
+  () => import("./features/songs/components/AddSongForm")
+);
+const EditSongForm = lazy(() => import("./features/songs/components/EditSong"));
+const ArtistsComponentWrapper = lazy(
+  () => import("./features/songs/components/ArtistsComponentWrapper")
+);
+const AlbumsComponentWrapper = lazy(
+  () => import("./features/songs/components/AlbumsComponentWrapper")
+);
+const GenresComponentWrapper = lazy(
+  () => import("./features/songs/components/GenresComponentWrapper")
+);
+const SongListsComponentWrapper = lazy(
+  () => import("./features/songs/components/SongListsComponentWrapper")
+);
+
+const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/songs" element={<SongListsComponentWrapper />} />
-        <Route path="/edit/:id" element={<EditSongForm />} />
-        <Route path="/newsong" element={<AddSongForm />} />
-        <Route path="/artists" element={<ArtistsComponentWrapper />} />
-        <Route path="/albums" element={<AlbumsComponentWrapper />} />
-        <Route path="/genres" element={<GenresComponentWrapper />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/songs"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <SongListsComponentWrapper />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <EditSongForm />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/newsong"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AddSongForm />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/artists"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ArtistsComponentWrapper />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/albums"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AlbumsComponentWrapper />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/genres"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <GenresComponentWrapper />
+            </Suspense>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
