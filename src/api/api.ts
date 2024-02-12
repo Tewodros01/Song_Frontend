c87336c22af6a16cfd33777308394691874e79b5
Tewodros01 <tewodrosm01@gmail.com>
2024-02-12 21:36:56 +0300

    Searching and Sorting

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,106 +5,92 @@ import { Artist } from "../types/artist";
 import { Statistics } from "../types/statistics";
 import { API_BASE_URL } from "../constants/constants";
 
-// Function to fetch all songs
-export const fetchSongsApi = async (): Promise<Song[]> => {
+interface ApiTypes {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  route: string;
+  payload?: any;
+}
+
+async function fetchFromApi<T>({
+  method,
+  route,
+  payload,
+}: ApiTypes): Promise<T> {
   try {
-    const response: AxiosResponse<Song[]> = await axios.get<Song[]>(
-      `${API_BASE_URL}/songs`
-    );
+    const response: AxiosResponse<T> = await axios({
+      method: method,
+      url: route,
+      data: payload,
+    });
+    console.log(response.data);
     return response.data;
   } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to fetch songs. Please try again later.");
+    throw new Error(
+      `Failed to fetch data from ${route}. Please try again later.`
+    );
   }
+}
+
+// Function to fetch all songs
+export const fetchSongsApi = async (): Promise<Song[]> => {
+  return fetchFromApi<Song[]>({
+    method: "GET",
+    route: `${API_BASE_URL}/songs`,
+  });
 };
 
 // Function to create a new song
 export const addSongApi = async (song: Song): Promise<Song> => {
-  try {
-    const response: AxiosResponse<Song> = await axios.post<Song>(
-      `${API_BASE_URL}/songs`,
-      song
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to create song. Please try again later.");
-  }
+  return fetchFromApi<Song>({
+    method: "POST",
+    route: `${API_BASE_URL}/songs`,
+    payload: song,
+  });
 };
 
 // Function to update an existing song
 export const updateSongApi = async (updatedSong: Song): Promise<Song> => {
-  try {
-    const response: AxiosResponse<Song> = await axios.put<Song>(
-      `${API_BASE_URL}/songs/${updatedSong.id}`,
-      updatedSong
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to update song. Please try again later.");
-  }
+  return fetchFromApi<Song>({
+    method: "PUT",
+    route: `${API_BASE_URL}/songs/${updatedSong.id}`,
+    payload: updatedSong,
+  });
 };
 
 // Function to delete a song
 export const deleteSongApi = async (songId: string): Promise<string> => {
-  try {
-    await axios.delete(`${API_BASE_URL}/songs/${songId}`);
-    return songId;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to delete song. Please try again later.");
-  }
+  await axios.delete(`${API_BASE_URL}/songs/${songId}`);
+  return songId;
 };
 
-// Function to fetch all Artisr
+// Function to fetch all Artists
 export const fetchSongsArtistApi = async (): Promise<Artist[]> => {
-  try {
-    const response: AxiosResponse<Artist[]> = await axios.get<Artist[]>(
-      `${API_BASE_URL}/songs/allArtist`
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to fetch songs. Please try again later.");
-  }
+  return fetchFromApi<Artist[]>({
+    method: "GET",
+    route: `${API_BASE_URL}/songs/allArtist`,
+  });
 };
 
-// Function to fetch all Songs AlbumApi
+// Function to fetch all Albums
 export const fetchSongsAlbumApi = async (): Promise<Album[]> => {
-  try {
-    const response: AxiosResponse<Album[]> = await axios.get<Album[]>(
-      `${API_BASE_URL}/songs/allAlbum`
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to fetch songs. Please try again later.");
-  }
+  return fetchFromApi<Album[]>({
+    method: "GET",
+    route: `${API_BASE_URL}/songs/allAlbum`,
+  });
 };
 
-// Function to fetch Genre
+// Function to fetch Genres
 export const fetchSongsGenresApi = async (): Promise<Album[]> => {
-  try {
-    const response: AxiosResponse<Album[]> = await axios.get<Album[]>(
-      `${API_BASE_URL}/songs/allGenre`
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to fetch songs. Please try again later.");
-  }
+  return fetchFromApi<Album[]>({
+    method: "GET",
+    route: `${API_BASE_URL}/songs/allGenre`,
+  });
 };
 
-// Function to fetch stastics
+// Function to fetch statistics
 export const fetchstaSticsApi = async (): Promise<Statistics> => {
-  try {
-    const response: AxiosResponse<Statistics> = await axios.get<Statistics>(
-      `${API_BASE_URL}/songs/statistics`
-    );
-    return response.data;
-  } catch (error) {
-    // Handle network errors or response errors
-    throw new Error("Failed to fetch songs. Please try again later.");
-  }
+  return fetchFromApi<Statistics>({
+    method: "GET",
+    route: `${API_BASE_URL}/songs/statistics`,
+  });
 };
